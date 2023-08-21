5cd11628f8e288955a032671652376bab623e6e2
ltsyyds <107777288+ltsyydswow@users.noreply.github.com>
2023-08-21 18:34:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,12 @@ sudo cp gpt /usr/local/bin/
 sudo chmod +x /usr/local/bin/gpt
 
 
+sudo apt-get update
+
+
+sudo apt-get install jq
+
+
 echo "gpt马上成功接入shell现在请添加一下您的api key"
 
 vi /usr/local/bin/gpt
